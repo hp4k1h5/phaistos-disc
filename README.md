a6561489bab5036d3eb5099d969a2e0e14ad717a
bob <robertwalks@gmail.com>
2024-11-21 08:06:24 -0800

    edit: fmt instead of print; docs
    include research in biblio

--- README.md
+++ README.md
@@ -2,25 +2,86 @@
 
 > A library of Python utilities to aid in the decipherment of the Phaistos disc
 
-![Sides A and B of the Phaistos disc](./src/data/phaistos_disk.png)
+![Sides A and B of the Phaistos disc](./src/data/phaistos-disc.png)
 
 Since 1908[^1], the Phaistos disc has eluded decipherment, despite widespread interest and several recent computer-aided analyses[^2]. The utilities provided in this library aim to assist in the analysis and decipherment of the Phaistos disc by providing a suite of tools and databases, and by documenting progress made by means of computational or analytical advancements.
 
 Datasets are currently limited to various transcriptions of the disc, and will eventually include word lists from known ancient Mediterranean languages.
 
+Some research materials are included in [/biblio/](./biblio)
+
 ## Usage
+
+### Basic text formatting
+
+```python
+# Print the disc with line+word numbering
+from phaistos_disc.util import format_disc
+
+print(format_disc("inside_out"))
+
+A1 𐇵 𐇒 𐇙
+A2 𐇐 𐇜
+A3 𐇤 𐇴 𐇲 𐇪 𐇪 𐇛 𐇑
+…
+
+# Print the disc with line separators
+print(format_disc(
+   "inside_out", letter_separator="-", word_separator="|", prefix=False,
+))
+𐇵-𐇒-𐇙|𐇐-𐇜|𐇤-𐇴-𐇲-𐇪-𐇪-𐇛-𐇑|𐇵-𐇒-𐇙|…
+```
+
+### Transcription
+
+As an example of the potential utility of the tool, I've included Achterberg's transcriptional mapping[^3] of phonetic values to their hieroglyphic forms in [](./src/data/phaistos-disc_signs-achterberg.csv), which can be used as shown below:
+
+```python
+# based on Achterberg 2021, _The Phaistos Disc: A Luwian Letter to Nestor_
+achterberg_transcription = format_disc(
+   "outside_in",
+   sign_map=get_sign_map(data_fp / "phaistos-disc_signs-achterberg.csv"),
+   output_type=OutputType.phoneme,
+   letter_separator="-",
+   word_separator="\n",
+   prefix=True,
+)
+print(achterberg_transcription)
+
+A1 á-tu-mi1-SARU-s6-ti
+A2 pa-ya-tu
+A3 u-ná-sa2-ti
+A4 u-u-ri
+A5 á-tu-hi-ya-wa8
+…
+```
+
+### Install
 
 1. Download the library, either by pip installing it, or cloning this repo
 
-- pip install phaistos-disc
-- git clone ...
+1.a) pip install phaistos-disc
+
+Or for contributors, fork or clone the repo
+
+1.b) git clone https://github.com/hp4k1h5/phaistos-disc.git
+
+phaistos-disc uses [`uv`](https://docs.astral.sh/uv/) to structure and manage the project.
+
+- [Install `uv`](https://docs.astral.sh/uv/getting-started/installation/)
+- `cd` into the project with e.g. `cd path/to/phaistos-disc`
+- Create a virtual environment: `uv venv`
+- Create a venv shell: `source .venv/bin/active`
+- Install dependencies: `uv install`
 
 ## Contributions
 
-Contributions are welcome. Fork the repo and submit a pull review.
+Contributions are welcome. Fork the repo, follow the installation instructions above, and submit a pull review with your edits.
 
 ## Bibliography
 
 [^1]: Evans, Arthur. 1952. _Scripta Minoa, the Written Documents of Minoan Crete, with Special Reference to the Archives of Knossos._ Oxford: Clarendon Press.
 
 [^2]: Braović, Maja, Damir Krstinić, Maja Štula, and Antonia Ivanda. 2024. “A Systematic Review of Computational Approaches to Deciphering Bronze Age Aegean and Cypriot Scripts.” Computational Linguistics 50 (2): 725–79. https://doi.org/10.1162/coli_a_00514.
+
+[^3]: Achterberg, Winfried. 2021. “The Phaistos Disc : A Luwian Letter to Nestor.” The Phaistos Disc : A Luwian Letter to Nestor, January. https://www.academia.edu/66972374/The_Phaistos_disc_a_Luwian_letter_to_Nestor.
--- src/phaistos_disc/util.py
+++ src/phaistos_disc/util.py
@@ -1,40 +1,99 @@
 import json
 import pathlib
+from enum import Enum
 
 import pandas as pd
 
 data_fp = pathlib.Path(__file__).parents[1] / "data"
 
-disc_to_path = {
-    "inside_out": data_fp / "phaistos-disc_inside-out.json",
-    "outside_in": data_fp / "phaistos-disc_outside-in.json",
-}
-sign_map = pd.read_csv(
-    data_fp / "phaistos-disc_signs.csv", index_col="No.", dtype="string"
-)
+
+class DiscToPath(Enum):
+    """The Phaistos disc can be read a number of ways:
+    Sides A and B are so named by convention, but one could read them in the
+    reverse order. Additionally, researchers disagree whether the disc should
+    be read from the center outwards (left-to-right) or from the disc's edge
+    toward the center (right-to-left). This library provides both readings in
+    ./src/data/ . This enum is a helper tool for accessing either
+    interpretation.
+    """
+
+    inside_out = data_fp / "phaistos-disc_inside-out.json"
+    outside_in = data_fp / "phaistos-disc_outside-in.json"
 
 
-def read_disc(disc_path):
+def get_sign_map(
+    path: pathlib.Path = data_fp / "phaistos-disc_signs.csv",
+):
+    sign_map: pd.DataFrame = pd.read_csv(path, index_col="No.", dtype="string")
+    return sign_map
+
+
+def read_disc(disc_path: pathlib.Path):
+    """Read a json formatted disc from file"""
+
     with open(disc_path, "r") as f:
         disc = json.loads(f.read())
     return disc
 
 
-def sign_to_word(sign_map: pd.DataFrame, number: str):
-    return sign_map.loc[number]["Symbol"]
+class OutputType(Enum):
+    number = "No."
+    symbol = "Symbol"
+    unicode = "Unicode"
+    phoneme = "Phoneme"
 
 
-def print_disc(disc):
-    disc_path = disc_to_path[disc]
+def number_to_symbol(
+    sign_map: pd.DataFrame, number: str, output_type: OutputType
+):
+    """Convert a zero-padded sign-number to unicode symbol.
+    Phaistos symbols are numbered 01-47 according to standard interpretation.
+    See ./src/data/phaistos-disc_signs.csv for complete listing.
+    """
+
+    return sign_map.loc[number][output_type.value]
+
+
+def format_disc(
+    disc: DiscToPath | pathlib.Path,
+    sign_map=get_sign_map(),
+    output_type: OutputType = OutputType.symbol,
+    letter_separator: str = " ",
+    word_separator: str = "\n",
+    prefix: bool = True,
+):
+    """Print a disc with unicode symbols with Side+wordcount prefix"""
+    disc_path = DiscToPath[disc].value
     disc = read_disc(disc_path)
-    sides = "side_a", "side_b"
+    sides = disc.keys()
+    output = []
     for side in sides:
         side_letter = side[-1].upper()
         for i, word in enumerate(disc[side]):
-            symbols = [sign_to_word(sign_map, sign) for sign in word]
-            print(f"{side_letter}{i+1} {' '.join(symbols)}")
+            symbols = [
+                number_to_symbol(sign_map, sign, output_type) for sign in word
+            ]
+            _prefix = f"{side_letter}{i+1} " if prefix else ""
+            output.append(f"{_prefix}{letter_separator.join(symbols)}")
+    return word_separator.join(output)
 
 
 if __name__ == "__main__":
     # print_disc("outside_in")
-    print_disc("inside_out")
+    # formatted_disc = format_disc(
+    #     "inside_out",
+    #     letter_separator="-",
+    #     word_separator="|",
+    #     prefix=False,
+    # )
+    # print(formatted_disc)
+
+    achterberg_transcription = format_disc(
+        "outside_in",
+        sign_map=get_sign_map(data_fp / "phaistos-disc_signs-achterberg.csv"),
+        output_type=OutputType.phoneme,
+        letter_separator="-",
+        word_separator="\n",
+        prefix=True,
+    )
+    print(achterberg_transcription)
