3d11d0d696804797c6921c96fe2a608c26c67a73
bob <robertwalks@gmail.com>
2025-07-19 11:38:26 -0700

    ci: move cltk to optional dep; ruff fix

--- src/phaistos_disc/__init__.py
+++ src/phaistos_disc/__init__.py
@@ -1,9 +1,4 @@
-from phaistos_disc.model import (
-    Direction,
-    DiscData,
-    OutputType,
-    PhaistosDisc,
-    SideOrdering,
-    data_fp,
-    read_sign_map,
-)
+# ruff: noqa: F401
+
+from phaistos_disc.model import (Direction, DiscData, OutputType, PhaistosDisc,
+                                 SideOrdering, data_fp, read_sign_map)
